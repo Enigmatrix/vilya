380d867771fbedaff9443bb0178c226a5f4a26a0
Enigmatrix <enigmatrix2000@gmail.com>
2024-04-28 19:17:53 +0800

    feat: bindings for io_uring syscall

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -15,3 +15,55 @@ mod bindings {
 }
 
 pub use bindings::*;
+
+use core::ffi;
+use std::io;
+
+fn resultify(ret: ffi::c_int) -> io::Result<ffi::c_int> {
+    if ret >= 0 {
+        Ok(ret)
+    } else {
+        Err(io::Error::from_raw_os_error(-ret))
+    }
+}
+
+pub unsafe fn io_uring_register(
+    fd: ffi::c_int,
+    opcode: ffi::c_uint,
+    arg: *const ffi::c_void,
+    nr_args: ffi::c_uint,
+) -> io::Result<ffi::c_int> {
+    resultify(sc::syscall4(
+        __NR_io_uring_register as usize,
+        fd as usize,
+        opcode as usize,
+        arg as usize,
+        nr_args as usize,
+    ) as _)
+}
+
+pub unsafe fn io_uring_setup(
+    entries: ffi::c_uint,
+    p: *mut io_uring_params,
+) -> io::Result<ffi::c_int> {
+    resultify(sc::syscall2(__NR_io_uring_setup as usize, entries as usize, p as usize) as _)
+}
+
+pub unsafe fn io_uring_enter(
+    fd: ffi::c_int,
+    to_submit: ffi::c_uint,
+    min_complete: ffi::c_uint,
+    flags: ffi::c_uint,
+    arg: *const ffi::c_void,
+    size: usize,
+) -> io::Result<ffi::c_int> {
+    resultify(sc::syscall6(
+        __NR_io_uring_enter as usize,
+        fd as usize,
+        to_submit as usize,
+        min_complete as usize,
+        flags as usize,
+        arg as usize,
+        size,
+    ) as _)
+}
