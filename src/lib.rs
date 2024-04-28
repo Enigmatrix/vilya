1fb6f6c371cf9ee92b15569f113ef42b558b4f8c
Enigmatrix <enigmatrix2000@gmail.com>
2024-04-28 22:15:11 +0800

    chore: simple read test

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,32 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
+    use std::{
+        borrow::Borrow,
+        error::Error,
+        fs::File,
+        os::fd::{AsFd, AsRawFd},
+    };
+
+    use io_uring::{opcode, types::Fd, IoUring};
+
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn basic_read() -> Result<(), Box<dyn Error>> {
+        let mut ring = IoUring::new(100)?;
+        let mut buf = [0; 1024];
+        let file = File::open("Cargo.toml")?;
+        let read = opcode::Read::new(Fd(file.as_raw_fd()), (&mut buf).as_mut_ptr(), 1024);
+        let read_entry = read.build();
+        unsafe { ring.submission().push(&read_entry)? }
+        ring.submit_and_wait(1)?;
+        let entry = ring.completion().next().expect("one read entry");
+        let result = entry.result();
+        if result < 0 {
+            return Err(std::io::Error::from_raw_os_error(-result as i32).into());
+        }
+        let buf = String::from_utf8_lossy(&buf[..result as usize]);
+        assert!(buf.contains("vilya"), "io_uring read failed");
+        Ok(())
     }
 }
